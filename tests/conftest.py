8e95fd12e4b4257c60d33d14dc1957f9d976c1a7
shahid <shahid.biitmcs@gmail.com>
2023-11-01 23:32:56 +0500

    new message

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,5 @@
 import pytest
-from selenium.webdriver import ActionChains, Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
 
 
 def pytest_addoption(parser):
@@ -17,18 +11,18 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="class")
 def setup(request):
     browser_name = request.config.getoption("browser_name")
-    if browser_name == "chrome":
-        request.driver = webdriver.Chrome()
-        #request.driver.get("https://rahulshettyacademy.com/angularpractice/")
+    chrome_options = webdriver.ChromeOptions()
 
+    if browser_name == "chrome":
+        # Add Chrome options here
+        chrome_options.add_argument("--start-maximized")  # Example: maximize the browser window
+        request.driver = webdriver.Chrome(options=chrome_options)
     elif browser_name == "firefox":
         request.driver = webdriver.Firefox()
-
     elif browser_name == "Edge":
         request.driver = webdriver.Edge()
 
     request.driver.get("https://rahulshettyacademy.com/angularpractice/")
-    request.driver.maximize_window()
     request.cls.driver = request.driver
 
     yield
